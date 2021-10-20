4492555492ebd42206a025e6e34f6430921e124e
Jawad63 <jawadqureshi111@gmail.com>
2021-10-20 14:06:16 +0200

    adding the skeleton of CYA project

---
+++ Babel/babel.js
@@ -0,0 +1,75 @@
+function horizontalify() {
+    const sections = document.getElementsByTagName("section");
+    let index = 0;
+    let top = 0;
+  
+    for (const section of sections) {
+      const currentIndex = index;
+      const currentTop = top;
+  
+      section.style.width = section.children.length * 100 + "vw";
+      section.style.height = "100vh";
+      section.style.position = "absolute";
+      section.style.left = 0;
+      section.style.top = currentTop + "px";
+  
+      makeHeightHolder(section);
+      watchHeightHolder(section, {
+        index: currentIndex,
+        top: currentTop
+      });
+  
+      index++;
+      top += section.nextSibling.clientHeight;
+    }
+  }
+  
+  function makeHeightHolder(node) {
+    const heightHolder = document.createElement("div");
+    heightHolder.style.height =
+      (node.clientWidth / node.children.length) * (node.children.length - 1) +
+      node.clientHeight +
+      "px";
+  
+    if (node.nextSibling) {
+      node.parentNode.insertBefore(heightHolder, node.nextSibling);
+    } else {
+      node.parentNode.append(heightHolder);
+    }
+  }
+  
+  function watchHeightHolder(node, props) {
+    const handleScroll = makeScrollHandler(node, props);
+  
+    window.addEventListener("scroll", handleScroll);
+  }
+  
+  function makeScrollHandler(node, props) {
+    const scrollableWidth = node.clientWidth - window.innerWidth;
+    let atTop = true;
+    let atBottom = false;
+  
+    return function handleScroll(e) {
+      const scrollTop = document.scrollingElement.scrollTop;
+      const left = scrollTop - props.top;
+      if (!atTop && scrollTop < props.top) {
+        node.style.position = "absolute";
+        node.style.left = 0;
+        node.style.top = props.top + "px";
+        atTop = true;
+      } else if (!atBottom && left > scrollableWidth) {
+        node.style.position = "absolute";
+        node.style.left = -1 * scrollableWidth + "px";
+        node.style.top = scrollableWidth + props.top + "px";
+        atBottom = true;
+      } else if (scrollTop > props.top && left <= scrollableWidth) {
+        node.style.position = "fixed";
+        node.style.left = -1 * left + "px";
+        node.style.top = 0;
+        atTop = false;
+        atBottom = false;
+      }
+    };
+  }
+  
+  addEventListener("load", horizontalify);
